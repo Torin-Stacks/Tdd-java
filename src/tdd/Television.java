d1d0496ac92638e13c32090e377d5b573b5e6ea1
Torin-Stacks <o.ajanaku@native.semicolon.africa>
2023-03-25 21:49:43 +0100

    initial commit

---
+++ src/tdd/Television.java
@@ -0,0 +1,60 @@
+package tdd;
+
+import java.util.Scanner;
+
+public class Television {
+
+    Scanner input = new Scanner(System.in);
+    boolean televisionIsOn;
+    int channelNumber;
+    int volume = 0;
+
+
+
+    public void toggleOn() {
+        if(televisionIsOn) {
+            televisionIsOn = false;}
+            else{
+                televisionIsOn = true;
+            }
+
+    }
+
+    public boolean isItOn() {
+        return televisionIsOn;
+    }
+
+    public void setChannelNumber(int channelNo) {
+        this.channelNumber = channelNo;
+
+    }
+
+
+    public int getChannelNumber() {
+        return channelNumber;
+    }
+
+
+    public void pastChannel() {
+        channelNumber = channelNumber - 1;
+    }
+
+    public void nextChannel() {
+        channelNumber = channelNumber + 1;
+    }
+
+
+    public void increaseVolume() {
+        if(volume < 100)
+        volume = volume + 1;
+    }
+
+    public void decreaseVolume() {
+        if(volume > 0)
+            volume = volume - 1;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
---
+++ test/tdd/TelevisionTest.java
@@ -0,0 +1,96 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TelevisionTest {
+
+    Television tv = new Television();
+    @Test
+
+    public void televisionIsOnTest(){
+        tv.toggleOn();
+        assertTrue(tv.isItOn());
+    }
+
+    @Test
+
+    public void televisionIsOffTest(){
+        tv.toggleOn();
+        tv.toggleOn();
+        assertFalse(tv.isItOn());
+    }
+
+   @Test
+
+    public void channelChangeTest(){
+        Television tv1 = new Television();
+        tv.setChannelNumber(20);
+        assertEquals(20, tv.getChannelNumber());
+    }
+
+
+    @Test
+
+    public void pastChannelTest(){
+        tv.setChannelNumber(20);
+        tv.pastChannel();
+        assertEquals(19, tv.getChannelNumber());
+    }
+
+    @Test
+    public void nextChannelTest(){
+        tv.setChannelNumber(20);
+        tv.nextChannel();
+        assertEquals(21, tv.getChannelNumber());
+    }
+
+
+    @Test
+    public void increaseVolumeTest(){
+        tv.getVolume();
+        tv. increaseVolume();
+        assertEquals(1, tv.getVolume());
+    }
+
+
+    @Test
+    public void decreaseVolumeTest(){
+
+        tv.getVolume();
+        tv.increaseVolume();
+        tv.increaseVolume();
+        tv.decreaseVolume();
+        assertEquals(1, tv.getVolume());
+    }
+
+    @Test
+    public void volumeNotIncreasedAbove100(){
+        tv.getVolume();
+        for(int count = 0; count <= 100; count++){
+            tv.increaseVolume();
+        }
+        assertEquals(100, tv.getVolume());
+    }
+
+
+    @Test
+    public void volumeNotDecreasedBelowZero(){
+        tv.getVolume();
+        tv.decreaseVolume();
+        assertEquals(0, tv.getVolume());
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    }
